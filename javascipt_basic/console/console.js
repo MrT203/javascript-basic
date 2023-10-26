111b8cd94eae801fc650862bacdb541005855d28
ANHTANDEPTRAI\Administrator <tanfctk@gmail.com>
2023-10-26 11:04:31 +0700

    string

--- javascipt_basic/console/console.js
+++
@@ -1,3 +0,0 @@
-// alert('Hello World from Javascript');
-console.log('Hello World from Javascript');
-console.log('Hello World from Tan');
---
+++ javascipt_basic/string/script.js
@@ -0,0 +1,8 @@
+let c = 'Tôi là 1 string';
+// Dung ` ` de in tat ca nhung gi co trong   ` `
+let d = `Toi la "Truong Tan" 
+        ! Xin chao ban`;
+
+
+console.log(c);
+console.log(d);
